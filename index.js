5678b2c881cbd2dc74d892fa8faffb5a94d9dffb
tychoengberink <rikie456@gmail.com>
2019-07-14 21:08:49 +0200

    edits

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
 /**
  * Created by rikie on 16-1-2018.
  */
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+
+const express = require('express');
+
+const Room  = require("./public/models/room");
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let connections = 0;
+let rooms = [];
 
 const PORT = 80;
 app.use(express.static(__dirname + "/public"));
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + 'public/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('Computer connected');
+io.on('connection', function (socket) {
+    connections++;
+    console.log(connections + ' Computer(s) connected');
 
-    socket.on('chat message', function(msg){
+    if (rooms.length === 0) {
+        io.emit("createroom");
+    }
+    socket.on("createroomname", function (msg) {
+        var room = new Room(rooms.length + 1, msg);
+        rooms.push(room);
+        io.emit("succesfullroomcreated", room);
+    });
+
+    socket.on('chatmessage', function (msg) {
+        console.log(msg);
         io.emit('message', msg);
     });
 
-    socket.on('disconnect', function(){
-        console.log('Computer disconnected');
+    socket.on('disconnect', function () {
+        connections--;
+        console.log('Computer disconnected ' + connections + " Remaining");
     });
 });
 
 
-
-http.listen(PORT, function(){
-    console.log('Confiured for Port ' + PORT);
+http.listen(PORT, function () {
+    console.log('Configured for Port ' + PORT);
 });
 
---
+++ public/models/room.js
@@ -0,0 +1,16 @@
+class Room {
+    constructor(id, name) {
+        this._id = id;
+        this._name = name;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+module.exports = Room;
